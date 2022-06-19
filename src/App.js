f423ff21b89e9a1ece926f929d58f9cf79dea4ca
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-06-19 17:03:14 +1200

    side bar styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-
+import { useState } from 'react'
+import './index.css'
+import { GiHamburgerMenu } from 'react-icons/gi'
 
 function App() {
-  return (
-    <div className="App">
 
+  const [activeMenu, setActiveMenu] = useState(false);
+
+  return (
+    <div className="app">
+      {activeMenu
+        ? (
+          <div
+            className="active_menu"
+            style={{ backgroundColor: '#fff' }}
+          >
+            <GiHamburgerMenu
+              onClick={() => setActiveMenu(!activeMenu)}
+            />
+          </div>
+        ) : (
+          <div className="no_active_menu">
+            <GiHamburgerMenu
+              onClick={() => setActiveMenu(!activeMenu)}
+            />
+          </div>
+        )
+      }
     </div>
   );
 }
