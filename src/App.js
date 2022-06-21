abd210e2f62aa243d411c8cfc5ad29791be5059b
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-06-21 21:39:58 +1200

    ?

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import { useState } from 'react'
 import Sidebar from './Sidebar/Sidebar'
 import Body from './Body/Body'
 import './index.css'
-// use state with titles to help for routing
-// use  api to generate dummy data, names etc...
+
 function App() {
   const [activeMenu, setActiveMenu] = useState(true);
 
