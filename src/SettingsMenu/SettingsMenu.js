66d6288a782057e855f79a452579851f8ff35eba
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-06-23 21:08:03 +1200

    settings markup

--- src/SettingsMenu/SettingsMenu.js
+++ src/SettingsMenu/SettingsMenu.js
@@ -4,7 +4,15 @@ import './SettingsMenu.css'
 const SettingsMenu = () => {
   return (
     <div className="settings_container">
-      this is some settings
+      <div className="settings_header">
+        <h2>Settings</h2>
+      </div>
+      <div className="settings_theme">
+
+      </div>
+      <div className="settings_colours">
+
+      </div>
     </div>
   )
 }
