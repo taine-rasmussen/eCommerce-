a887b0aef152ff3f3a6eb5046b3653bef9701527
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-06-28 21:16:40 +1200

    please just work

--- src/KanBan/KanBan.js
+++ src/KanBan/KanBan.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 // import { KanbanComponent, ColumnsDirective, ColumnDirective } from '@syncfusion/ej2-react-kanban';
+import { KanbanComponent, ColumnsDirective, ColumnDirective } from "@syncfusion/ej2-react-kanban";
 import { kanbanData, kanbanGrid } from '../data/dummy';
 // import { Header } from '../components';
 
@@ -9,7 +10,16 @@ const KanBan = () => {
 
   return (
     <div>
-
+      <KanbanComponent
+        id="kanban"
+        keyField="Status"
+        dataSource={kanbanData}
+        cardSettings={{ contentField: 'Summary', headerField: 'Id' }}
+      >
+        <ColumnsDirective>
+          {kanbanGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+      </KanbanComponent>
     </div>
   )
 }
