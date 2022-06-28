7d1bb248f7e7ba287d42db50d74510e75c9b3ff0
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-06-28 21:00:40 +1200

    deleted a bunch

--- src/KanBan/KanBan.js
+++ src/KanBan/KanBan.js
@@ -1,38 +1,12 @@
-import React from 'react'
-import Board from '@asseinfo/react-kanban'
-import '@asseinfo/react-kanban/dist/styles.css'
+import { useState } from 'react'
+
 
 const KanBan = () => {
 
-  const board = {
-    columns: [
-      {
-        id: 1,
-        title: 'Backlog',
-        cards: [
-          {
-            id: 1,
-            title: 'Add card',
-            description: 'Add capability to add a card in a column'
-          },
-        ]
-      },
-      {
-        id: 2,
-        title: 'Doing',
-        cards: [
-          {
-            id: 2,
-            title: 'Drag-n-drop support',
-            description: 'Move a card between the columns'
-          },
-        ]
-      }
-    ]
-  }
+
+
   return (
     <div>
-      <Board initialBoard={board} />
 
     </div>
   )
